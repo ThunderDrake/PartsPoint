fdf8fdaaf0ee4da02d8ca2bccab67745ba36a3e9
ThunderDrake <drake9100@mail.ru>
2022-05-12 12:10:25 +0300

    add: Добавил остановку виджета социальных сетей перед футером

--- src/js/components/scroll-link.js
+++ src/js/components/scroll-link.js
@@ -1,5 +1,29 @@
 import SmoothScroll from 'smooth-scroll';
+
+const socialWidget = document.querySelector('.social-widget');
+const footer = document.querySelector('.footer');
+
 const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 2500,
 	speedAsDuration: true
+});
+
+function checkOffset() {
+  function getRectTop(el){
+    const rect = el.getBoundingClientRect();
+    return rect.top;
+  }
+
+  if((getRectTop(socialWidget) + document.body.scrollTop) + socialWidget.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 10) {
+    // socialWidget.style.position = 'absolute';
+    socialWidget.style.bottom = footer.offsetHeight + 10 + 'px';
+  }
+  if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop)) {
+    socialWidget.style.position = 'fixed'; // restore when you scroll up
+    socialWidget.style.bottom = '5%';
+  }
+}
+
+document.addEventListener("scroll", function(){
+  checkOffset();
 });
