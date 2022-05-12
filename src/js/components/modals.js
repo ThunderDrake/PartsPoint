3049d8bda71853dbe51bc54861c9289f5cbf8f81
ThunderDrake <drake9100@mail.ru>
2022-05-12 13:00:47 +0300

    fix: Пофиксил закрытие модального окна с видео

--- src/js/components/modals.js
+++ src/js/components/modals.js
@@ -1,2 +1,14 @@
 import GraphModal from 'graph-modal';
-const modal = new GraphModal();
+const modal = new GraphModal({
+  isOpen: (modal) => {
+    const modalVideo = modal.modalContainer.querySelector('.modal__video').getAttribute('data-video');
+    const iframe = modal.modalContainer.querySelector('iframe');
+
+    iframe.setAttribute('src', modalVideo)
+  },
+  isClose: (modal) => {
+    const iframe = modal.modalContainer.querySelector('iframe');
+
+    iframe.setAttribute('src', '')
+  }
+});
